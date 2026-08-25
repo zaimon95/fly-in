3296a76322c86d8399b8a5d6ee63b5e4fb09a08b
Zaimon <simon14101995@gmail.com>
2026-08-25 15:39:02 +0200

    feat: modification classe Metadata + creation classe Drones

--- parser.py
+++ parser.py
@@ -1,25 +1,37 @@
-class Hub_Metadata:
+class Metadata:
+
     def __init__(self, cond: str, value: str | int) -> None:
         self.cond = cond
         self.value = value
 
 
 class Hub:
-    def __init__(self, name: str, x: int, y: int, meta: Hub_Metadata) -> None:
+
+    def __init__(self, name: str, x: int, y: int, meta: Metadata) -> None:
         self.name = name
         self.x = x
         self.y = y
         self.meta = meta
 
 
-class Con_Metadata:
-    def __init__(self, cond: str, value: int) -> None:
-        self.cond = cond
-        self.value = value
-
-
 class Connection:
-    def __init__(self, hub1: str, hub2: str, meta: Con_Metadata) -> None:
+
+    def __init__(self, hub1: str, hub2: str, meta: Metadata) -> None:
         self.hub1 = hub1
         self.hub2 = hub2
         self.meta = meta
+
+
+class Drones:
+
+    def __init__(self,
+                 name: str,
+                 x: int,
+                 y: int,
+                 arrived: bool,
+                 inHub: bool) -> None:
+        self.name = name
+        self.x = x
+        self.y = y
+        self.arrived = False
+        self.inHub = True
